fdd49ea342314ec539a86d3db59904a26afe6624
Brandon Bernie <bmbernie@gmail.com>
2016-05-05 00:24:38 -0700

    removed some of the boilerplate license at the top of files, got rid of some useless scripts

--- setup/osx.sh
+++ setup/osx.sh
@@ -1,33 +1,10 @@
 #!/usr/bin/env bash
 #
-# The MIT License (MIT)
-#
-# Copyright (c) 2013 bmbernie
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
 #############################################################################
-# Author: Brandon Bernie
-# Email : bmbernie@gmail.com
-# File  : config.osx
-# OS    : Mac OS X [Tested on Mavericks]
-# Origin: ~/.osx - http://mths.be/osx
-# What  : All that post install configuration stuff 
+# Brandon Bernie
+# bmbernie@gmail.com
+# 
+# Post install configuration for OS X 
 #############################################################################
 
 # Ask for the administrator password upfront
@@ -237,7 +214,7 @@ sudo mdutil -i on / > /dev/null
 sudo mdutil -E / > /dev/null
 
 ###############################################################################
-# Terminal & iTerm 2                                                          #
+# Terminal                                                                    #
 ###############################################################################
 
 echo -e "Set the terminal ..."
