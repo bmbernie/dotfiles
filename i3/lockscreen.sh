a337b0babfc08ca3ed03fed054b4a124dfcb9d9b
Brandon Bernie <bmbernie@gmail.com>
2017-02-20 17:49:51 -0800

    added named workspaces

--- i3/lockscreen.sh
+++ i3/lockscreen.sh
@@ -3,7 +3,7 @@
 TMPFILE=$(mktemp /tmp/lockscreen.XXXXXXXXXXXXXXXXXXXXXXXXXXXX.png)
 LOCKPNG="/home/bmb/dotfiles/i3/lock.png"
 scrot $TMPFILE
-convert $TMPFILE -scale 10% -scale 1000% $TMPFILE
-[[ -f $LOCKPNG ]] && convert $TMPFILE $LOCKPNG -gravity center -composite -matte $TMPFILE
+convert $TMPFILE -scale 10% -scale 1000% -colorize 25% $TMPFILE
+[[ -f $LOCKPNG ]] && convert $TMPFILE $LOCKPNG -gravity center -annotate +0+0 'Type Password to Unlock' -composite -matte $TMPFILE
 i3lock -u -i $TMPFILE
 rm $TMPFILE
